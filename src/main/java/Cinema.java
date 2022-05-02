d11297dc884effccf2df3dc62344e2a1ae96bfdf
MarcusC67 <93521177+MarcusC67@users.noreply.github.com>
2022-05-02 14:27:20 +0100

    Green - 5th test checkAllocatedTwoLotsOfThreeSeatsA1toB1()

--- src/main/java/Cinema.java
+++ src/main/java/Cinema.java
@@ -1,50 +1,53 @@
 public class Cinema {
 
+    private String str;
     private int seatsAvailable;
-    private String returnStr;
+    String returnStr;
 
-    int row = 1;
-    int seat = 1;
-    int j = 0;
+    private int j = 0;
+    int row = 0;
+    int seat = 0;
+    char rowLetter = 'A';
+    String rows = "ABC";
+
+    StringBuilder seatsBooked = new StringBuilder();
 
     // constructor
     public Cinema() {
         this.seatsAvailable = 15;
-        String rows = "ABC";
     }
 
     public String allocateSeats(int seatsRequested) {
-
-        StringBuilder seatsBooked = new StringBuilder();
-        String str = null;
 
         if (seatsAvailable == 0) {
             return "Sorry, no seats available!";
         } else {
 
-            // loop through each requested seat 1-3
-            for (int i = 0; i < seatsRequested; i++) {
-                j++;
-                str = String.valueOf(seatsBooked.append(allocateEachSeat(row, j)).append(" "));
-                str = str.substring(0, str.length() - 1); // strip out last space
-                seatsAvailable--;
+           // loop through each requested seat 1-3
+           for (int k = 0; k < seatsRequested; k++) {
+               j++;
+               // row is 0 - need update row - call method set row
+               str = String.valueOf(seatsBooked.append(allocateOneSeat(row, j)).append(" "));
+               seatsAvailable--;
             }
         }
+        str = str.substring(0, str.length() - 1); // strip out last space
         return str; // return string of all booked seats
     }
 
-    public String allocateEachSeat(int row, int seat) {
-
-        char rowLetter = "ABC".charAt(row-1);
+    public String allocateOneSeat(int row, int seat) {
 
         if (seat == 5) {
-            row ++;
-            seat = 1;
             returnStr = rowLetter + String.valueOf(seat);
+            row ++;
+            rowLetter = rows.charAt(row);
+            seat = 1;
+            j = 0;
         } else {
             returnStr = rowLetter + String.valueOf(seat);
             seat ++;
         }
         return returnStr; // return individual row and seat eg: A1
     }
+
 }
--- src/test/java/CinemaTest.java
+++ src/test/java/CinemaTest.java
@@ -1,4 +1,5 @@
 import org.junit.Ignore;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import static org.junit.Assert.assertEquals;
 
@@ -13,28 +14,44 @@ public class CinemaTest {
         Cinema seats = new Cinema();
         // Assert
         assertEquals(expected, seats.allocateSeats(3));
-    } */
+    }
 
-    /* @Ignore
+    @Ignore
     @Test
     public void checkIfCinemaFull() {
         int expected = 0;
         Cinema seats = new Cinema();
         assertEquals(expected, seats.allocateSeats(3));
-    } */
+    }
 
     @Test
     public void checkAllocatedSingleSeatA1() {
         String expected = "A1";
         Cinema seats = new Cinema();
-        assertEquals(expected, seats.allocateSeats(1));
-    }
+        Assertions.assertEquals(expected, seats.allocateSeats(1));
+    } */
 
     @Test
     public void checkAllocatedThreeSeatsA1toA3() {
         String expected = "A1 A2 A3";
         Cinema seats = new Cinema();
-        assertEquals(expected, seats.allocateSeats(3));
+        Assertions.assertEquals(expected, seats.allocateSeats(3));
+    }
+
+    @Test
+    public void checkAllocatedTwoLotsOfThreeSeatsA1toB1() {
+        int seatsRequired;
+        String actual = null;
+        String expected = "A1 A2 A3 A4 A5 B1";
+        int[] requested = {3, 3};
+
+        Cinema seats = new Cinema();
+        for (int i = 0; i <= 1; i++) {
+            seatsRequired = requested[i];
+            actual = seats.allocateSeats(seatsRequired);
+        }
+        System.out.println("Allocated the following seats = " + actual);
+        Assertions.assertEquals(expected, actual);
     }
 
 }
